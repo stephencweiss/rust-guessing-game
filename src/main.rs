09758c0db32cbb14160fb3ee7676b8b79235fbbd
Stephen <stephen.c.weiss@gmail.com>
2020-03-29 18:36:46 -0500

    feat: add loop for multiple guesses
    now the user can try to guess multiple times to find the _right_
    answer

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,23 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
-
-    // recast the guess thanks to shadowing, initially captured as a string from the user, as a number
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a parseable number!");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!"),
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        // recast the guess thanks to shadowing, initially captured as a string from the user, as a number
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a parseable number!");
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => println!("You Win!"),
+        }
     }
 }
