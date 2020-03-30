9cc1b00c54b750c89da93bd096f91be662017e43
Stephen <stephen.c.weiss@gmail.com>
2020-03-29 19:17:55 -0500

    feat: add a guess count

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
+    let mut guess_count = 0;
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -22,13 +23,13 @@ fn main() {
                 continue;
             }
         };
-
-        println!("You guessed: {}", guess);
+        guess_count += 1;
+        println!("You guessed: {}. You have made {} guesses", guess, guess_count);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too Small!"),
             Ordering::Greater => println!("Too Big!"),
             Ordering::Equal => {
-                println!("You Win!");
+                println!("You Win! It took you {} guess(es)", guess_count);
                 break;
             }
         }
